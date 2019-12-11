f9c9920afff83b856b53a1913ee1a97e197da617
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-11 15:45:28 -0500

    set up reducer, mapped state to props, changed state to props now  everything displays correctly in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+const App = props => {
   // const state = {
   //   additionalPrice: 0,
   //   car: {
@@ -35,16 +36,26 @@ const App = () => {
     <div className="boxes">
       <div className="box">
         {/* this is the left side(with the car image) */}
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
         {/* this is the right side(with the add buttons) */}
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  console.log('state from mapStateToProps in App', state)
+  return {
+    additionalPrice: state.additionalPrice, 
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  }
+}
+
+
+export default connect(mapStateToProps, {})(App);
